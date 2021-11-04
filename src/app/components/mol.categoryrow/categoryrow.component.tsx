bdb1cd61feea89d314c0b5a3b4d8608a119811af
AdrianAmerico <1fnadrian1@gmail.com>
2021-11-03 21:41:47 -0300

    adicionado navegadores laterais no row de filmes

--- src/app/components/mol.categoryrow/categoryrow.component.tsx
+++ src/app/components/mol.categoryrow/categoryrow.component.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { API } from '../../../atomic'
-import { Card, StyledList, StyledListItem, CategoryContainer } from './categoryrow.component.style'
+import { NavigateLeftIcon } from '../mol.navigatelefticon'
+import { NavigateRightIcon } from '../mol.navigaterighticon'
+import { Card, StyledList, StyledListItem, CategoryContainer, DivNavigateIconStyled } from './categoryrow.component.style'
 
 interface Props {
     title: string;
@@ -8,11 +10,38 @@ interface Props {
 }
 
 export const CategoryRow = ({ title, item }: Props) => {
+    const [scrollX, setSctrollX] = React.useState<number>(0)
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0
+        }
+        setSctrollX(x)
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        const listWidth = item?.items?.length * 150
+        if ((window.innerWidth - listWidth) > x) {
+            x = (window.innerWidth - listWidth)
+        }
+        setSctrollX(x)
+    }
+
     return (
         <CategoryContainer>
             <h2>{title}</h2>
-            <Card>
-                <StyledList>
+            <DivNavigateIconStyled side="left" onClick={handleLeftArrow}>
+                <NavigateLeftIcon />
+            </DivNavigateIconStyled>
+
+            <DivNavigateIconStyled side="right" onClick={handleRightArrow}>
+                <NavigateRightIcon />
+            </DivNavigateIconStyled>
+
+            <Card size={item?.items?.length * 150}>
+                <StyledList margin={scrollX}>
                     {item?.items?.map((data: any, index: number) => {
                         return (
                             <StyledListItem key={index}>
